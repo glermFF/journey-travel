042bed7e50fc29fae4d41645d64dcfd09c6948b7
glermFF <gffreitas13@gmail.com>
2024-07-10 16:36:09 -0300

    "venv, routes, server"

---
+++ src/main/routes/trips_routes.py
@@ -0,0 +1,10 @@
+from flask import jsonify, Blueprint
+
+# Criação de uma rota;
+
+trips_routes_bp = Blueprint("trip_route", __name__)
+
+# 
+@trips_routes_bp.route("/trips", methods=["POST"])
+def create_trip():
+    return jsonify({"salve": "pessoal"}), 200 
